6b809ffca07836e6665c17eb1057c5779ed488ee
Lounes Badji <lounes.b.ext@padoa-group.com>
2022-09-09 23:49:56 +0200

    feat: swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { TrainingModule } from './business/training/application/training.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [TrainingModule],
+  imports: [CoreModule, TrainingModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({ origin: app.get(ConfigService).get('CORS_ORIGIN') });
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('DzDialect Training API')
+    .setDescription('DzDialect Training API Documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup('openapi', app, document, {
+    swaggerOptions: {
+      displayRequestDuration: true,
+      docExpansion: 'none',
+      operationsSorter: 'alpha',
+      tagsSorter: 'alpha',
+    },
+  });
+
+  await app.listen(app.get(ConfigService).get('PORT') || 3000);
 }
 bootstrap();
