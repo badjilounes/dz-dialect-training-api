340b661ba066cfb2e779e0ad18bd190facb65218
Lounes Badji <lounes.b.ext@padoa-group.com>
2022-09-11 23:40:39 +0200

    chore: test setup

--- jest.config.e2e.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require('./jest.config.js');
-
-module.exports = {
-  ...baseConfig,
-  testRegex: '\\.e2e-spec\\.ts$',
-  coverageDirectory: './coverage/e2e',
-  globalSetup: './test/setup/index.ts',
-};
